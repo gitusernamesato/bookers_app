f9948dbd547714646a67c01d50f49e4faf1dfb91
EC2 Default User <shinya.sin09@gmail.com>
2022-06-13 07:45:45 +0000

    [update]flashmessage

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -5,12 +5,16 @@ class BooksController < ApplicationController
 
   def create
     book = Book.new(book_params)
-    book.save
-    redirect_to book_path(book.id)
+    if book.save
+      redirect_to book_path(book.id), notice: "Book was successfully create."
+    else
+      render :index
+    end
   end
 
   def index
-    @book = Book.all
+    @books = Book.all
+    @book = Book.new
   end
 
   def show
@@ -18,13 +22,28 @@ class BooksController < ApplicationController
   end
 
   def edit
+    @book = Book.find(params[:id])
   end
 
+  def update
+    book = Book.find(params[:id])
+    if book.update(book_params)
+      redirect_to book_path(book.id), notice: "Book was successfully update."
+    else
+      render :index
+    end
+  end
+
+  def destroy
+   book = Book.find(params[:id])
+   book.destroy
+   redirect_to books_path
+  end
 
  private
 
   def book_params
-   params.require(:book).permit(:title, :body, :image)
+   params.require(:book).permit(:title, :body)
   end
 
 end
