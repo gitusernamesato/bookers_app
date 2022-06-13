6046101584182f2f3284ab9b751d4817133958c9
EC2 Default User <shinya.sin09@gmail.com>
2022-06-13 11:30:22 +0000

    [update]errormessages

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -4,9 +4,10 @@ class BooksController < ApplicationController
   end
 
   def create
-    book = Book.new(book_params)
-    if book.save
-      redirect_to book_path(book.id), notice: "Book was successfully create."
+    @book = Book.new(book_params)
+    @books = Book.all
+    if @book.save
+      redirect_to book_path(@book.id), notice: "Book was successfully create."
     else
       render :index
     end
@@ -26,11 +27,11 @@ class BooksController < ApplicationController
   end
 
   def update
-    book = Book.find(params[:id])
-    if book.update(book_params)
-      redirect_to book_path(book.id), notice: "Book was successfully update."
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+       redirect_to book_path(@book.id), notice: "Book was successfully update."
     else
-      render :index
+      render :edit
     end
   end
 
